507dda7ac7e8e486f9100d7ff72981b44af955f8
Raymond efa <ereta19@yahoo.com>
2023-02-25 16:32:02 +0000

    app project updated

--- app.py
+++ app.py
@@ -10,3 +10,4 @@ remote: Resolving deltas: 100% (1/1), completed with 1 local object.
 To https://github.com/rayeeta/tesla-project
    0d629fa..3e80cfa  master -> master
 
+Here, we first accept the filename from the user using the read command. Then, we check if the file exists using the -e option of the test command. If the file exists, we sort the file using the sort command and redirect the output to a new file with _sorted suffix. Finally, we display a message indicating that the file was sorted successfully and the name of the sorted file. If the file does not exist, we display a user-friendly error message asking the user to check the filename and try again.
