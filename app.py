a361c2796ad33747e3264b00129357c36195284c
Raymond efa <ereta19@yahoo.com>
2023-02-25 16:39:27 +0000

    saturday app update

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 Today is sunday the 25.02.2023 and it is left just 3 days for us to get into the new moth of March 2023
 Jesus is Lord
 The Lord is my Sheperd forever
+
 Enumerating objects: 5, done.
 Counting objects: 100% (5/5), done.
 Compressing objects: 100% (3/3), done.
-Writing objects: 100% (3/3), 333 bytes | 333.00 KiB/s, done.
-Total 3 (delta 1), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+Writing objects: 100% (3/3), 333 bytes 
+333.00 KiB/s, done.
+Total 3 (delta 1), reused 0 
+(delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), 
+completed with 1 local object.
 To https://github.com/rayeeta/tesla-project
-   0d629fa..3e80cfa  master -> master
+0d629fa..3e80cfa  master -> master
 
-Here, we first accept the filename from the user using the read command. Then, we check if the file exists using the -e option of the test command. If the file exists, we sort the file using the sort command and redirect the output to a new file with _sorted suffix. Finally, we display a message indicating that the file was sorted successfully and the name of the sorted file. If the file does not exist, we display a user-friendly error message asking the user to check the filename and try again.
+Here, we first accept the filename 
+from the user using the read command. 
+Then, we check if the file exists using 
+the -e option of the test command. If the file exists, 
+we sort the file using the sort command and 
+redirect the output to a new file with _sorted suffix. 
+Finally, we display a message indicating that 
+the file was sorted successfully and the 
+name of the sorted file. If the file does not exist, 
+we display a user-friendly error message 
+asking the user to check the filename and try again.
+
+Ray project ends
+
